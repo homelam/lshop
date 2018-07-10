ceb5389e14624036ae060796216c9d325b765b7a
hongliang <960791428@qq.com>
2018-07-10 20:44:09 +0800

    商品列表添加搜索以及排序

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,9 +9,43 @@ class ProductsController extends Controller
 {
     //
     public function index(Request $request)
-    {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
+    {   
+        // 创建一个查询构造器
+        $builder = Product::query()->where('on_sale', true);
+        
+        // 判断是否有提交 search 参数，如果有就赋值给 $search 变量
+        // search 参数用来模糊搜索商品
+        if ($search = $request->input('search', '')) {
+            $like = '%'.$search.'%';
+            // 模糊收索商品名称，商品详情，SKU，标题，SKU描述
+            $builder->where(function($query) use ($like) {
+                $query->where('name', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function($query) use ($like) {
+                        $query->where('name', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+        }
 
-        return view('products.index', ['products' => $products]);
+        // 是否有提交排序 参数
+        if ($order = $request->input('order', '')) {
+            // 是否是以 _asc 或者 _desc结尾
+            if (preg_match('/^(.+)_(asc|desc)/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+        
+        $products = $builder->paginate(16);
+
+        return view('products.index', [
+            'products' => $products,
+            'filters' => [
+                'search' => $search,
+                'order' => $order,
+            ],
+        ]);
     }
 }
